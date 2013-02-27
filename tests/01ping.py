a1c5079e4ea1f76c668da540f6a5ef61976cd3b2
K Jonathan Harker <k.jonathan.harker@hp.com>
2013-02-26 18:41:52 -0800

    Wait for the servers to start pinging, and keep track of how long it took.

--- tests/01ping.py
+++ tests/01ping.py
@@ -1,13 +1,30 @@
 import subprocess
-
+import time
 
 def run(servers, **kwargs):
     print('ping test')
-    for ip in [ servers[x]['ip'] for x in servers.keys() ]:
-        try:
-            subprocess.check_call(['ping', '-c 3', ip])
-        except Exception as e:
-            print(e)
 
-    print("ping successful")
+    times = {}
+    count = 1
+    max_count = 30
+    sleep_time = 1
+    ips = [ servers[x]['ip'] for x in servers.keys() ]
 
+    while count < max_count:
+        procs = []
+        for ip in ips:
+            if ip not in times:
+                procs.append(subprocess.Popen(['ping', '-c 1', ip],
+                                              stdout=subprocess.PIPE,
+                                              stderr=subprocess.PIPE))
+        for proc in procs:
+            proc.communicate()
+            if proc.returncode is 0:
+                print("ping successful: {0}".format(ip))
+                times[ip] = count * sleep_time
+        time.sleep(sleep_time)
+        count += 1
+
+    for ip in ips:
+        if ip not in times:
+            raise Exception("Could not ping {0}".format(ip))
